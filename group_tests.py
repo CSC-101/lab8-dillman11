ca6ce789b6cdb59ea4a69814f4033b6bd73d8e35
Dillon Schaffer <dschaffe@calpoly.edu>
2024-11-15 17:56:29 -0800

    Lab 8

---
+++ group_tests.py
@@ -0,0 +1,30 @@
+import unittest
+
+import group
+
+
+class TestCases(unittest.TestCase):
+
+    def test_groups_of_3(self):
+        input = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        result = group.groups_of_3(input)
+        expected = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        self.assertEqual(expected,result)
+
+    def test_groups_of_3_2(self):
+        input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        result = group.groups_of_3(input)
+        expected = [[1, 2, 3], [4, 5, 6], [7, 8, 9],[10]]
+        self.assertEqual(expected,result)
+
+    def test_groups_of_3_3(self):
+        input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        result = group.groups_of_3(input)
+        expected = [[1, 2, 3], [4, 5, 6], [7, 8, 9],[10, 11]]
+        self.assertEqual(expected,result)
+
+    def test_groups_of_3_4(self):
+        input = []
+        result = group.groups_of_3(input)
+        expected = []
+        self.assertEqual(expected,result)
